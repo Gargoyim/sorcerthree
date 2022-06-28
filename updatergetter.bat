39521666b3bfd50e1f42a3e1fce20770002077ce
Gargoyim <MVail95@Yahoo.com>
2022-06-27 21:28:25 -0500

    Add files via upload

---
+++ updatergetter.bat
@@ -0,0 +1,5 @@
+ECHO OFF
+ECHO Checking for updates...
+powershell -noprofile -command "(New-Object Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/Gargoyim/sorcerthree/master/updater.bat\", 'updater.bat')"
+call sorcerupdater.bat
+EXIT
